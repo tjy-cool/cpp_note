971f8f39064fea520ebc478a47f0d43dca625f09
tjy-cool <990423909@qq.com>
2018-12-30 23:26:10 +0800

    add MyDebug.h

---
+++ Log/MyLog/MyDebug.h
@@ -0,0 +1,12 @@
+/************************************************* 
+Copyright:
+Author: tjy-cool
+Date:2018-12-30
+Description: 这是一个最简单的日志，完全利用宏定义实现，
+             功能简单，主要用于工程的简单调试
+**************************************************/  
+#ifndef __MYDEBUG_H_
+#define __MYDEBUG_H_
+
+
+#endif  // end of #ifndef __MYDEBUG_H_
